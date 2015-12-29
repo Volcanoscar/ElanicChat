3ad1a4bb2919c598c43d81d283509d2074564331
ppsreejith <mailz4sreejith@gmail.com>
2015-12-29 17:47:12 +0530

    Added controllers for chat and utility.
    Controller chat allows a single, unified interface to access the
    database for the main app. util.js is a collection of helper functions
    used elsewhere in the app.

---
+++ controllers/util.js
@@ -0,0 +1,33 @@
+/*
+Some helper functions
+*/
+
+var _DEBUG = 1;
+
+module.exports = {
+    log : function(data) {
+	if (_DEBUG == 0)
+	    return;
+	else if (_DEBUG == 1)
+	    console.log(data);
+	else if (_DEBUG == 2)
+	    console.error(data);
+    },
+
+    API : {
+	SEND : "TYPE_SEND",
+	GET : "TYPE_GET",
+	FAIL : false,
+	SUCCESS : true,
+	ERROR : 'error'
+    },
+
+    error_msg : function(err, msg) {
+	return {
+	    success : this.API.FAIL,
+	    error : err,
+	    data : msg
+	};
+    }
+
+};
